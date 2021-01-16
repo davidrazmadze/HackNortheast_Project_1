73620a6afd08c4e4d4dcb91d185292ab0e4ac895
David Razmadze <razd45@gmail.com>
2021-01-16 14:46:04 -0500

    Update

--- getLiveTweet.py
+++ getLiveTweet.py
@@ -9,6 +9,7 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
+
 class tweety(tweepy.StreamListener):
     def __init__(self, text1, likes1, retweets1, user1, follower1):
         text = text1                # text of tweet
@@ -17,7 +18,10 @@ class tweety(tweepy.StreamListener):
         user = user1                # username of tweeter
         follower = follower1        # follower count of user who posted tweet
 
+
 global x
+
+
 class MyStream(tweepy.StreamListener):
     def on_status(self, status):
         print(status.text)
@@ -25,13 +29,13 @@ class MyStream(tweepy.StreamListener):
         print(status.user.followers_count)
         print(status.favorite_count)
         print(status.retweet_count)
-        global mystream_Text, mystream_Favorite_Count, mystream_retweet_count, mystream_user_id_str, mystream_Follower_Count 
+        global mystream_Text, mystream_Favorite_Count, mystream_retweet_count, mystream_user_id_str, mystream_Follower_Count
         #x = tweety(status.text, status.favorite_count, status.retweet_count, status.user.id_str, status.user.followers_count)
         x = status.text
         return False
 
 
 mstream = MyStream()
-myStreamListen = tweepy.Stream(auth = api.auth, listener = mstream)
+myStreamListen = tweepy.Stream(auth=api.auth, listener=mstream)
 myStreamListen.filter(follow=["1292952619611295763"])
 print(x)
