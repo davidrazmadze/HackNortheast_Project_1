d7a9f09df1b3d308a9c52a211b92e08a4a39d3fa
Daniel Tkachov <Daniel_Tkachov@student.uml.edu>
2021-01-16 11:49:45 -0500

    Formalize, soon to-do is to push out tweets

--- getPastTweets.py
+++ getPastTweets.py
@@ -10,22 +10,30 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-ticker = input("Enter Stock Ticker: ")
-numTweet = 6
-global tick
-with open('HackNortheast_Project_1\\t2n.txt') as file:
-    for line in file:
-        if ticker in line:
-            print(f'{ticker} FOUND')
-            tick = line
+def get_relevant_tweets(ticker, number_of_tweets):
+    tweet_list = [] # list to hold tweets fetched
+    global tick
+    with open('HackNortheast_Project_1\\t2n.txt') as file:
+        for line in file:
+            if ticker in line:
+                tick = line
+    for tweet in tweepy.Cursor(api.search, q=tick, lang='en').items(number_of_tweets):
+        try:
+            print(f"Tweet received; Text: \n{tweet.text} ---- {tweet.user.screen_name}")
+            tweet_list.append(tweet)
+            if ticker in tweet.text:
+                print(f"{ticker} FOUND")
+                
+        except tweepy.error.TweepError as er:
+            print(er.reason)
+        except StopIteration:
+            break
+    return tweet_list
+def store_tweets_in_json(tweet_list, file='relevant_tweets.json'):
+    
 
-print("===================")
-for tweet in tweepy.Cursor(api.search, q=tick, lang='en').items(numTweet):
-    try:
-        print(f"Tweet received; Text: \n{tweet.text} ---- {tweet.user.screen_name}")
-        if ticker in tweet.text:
-            print(f"{ticker} FOUND")
-    except tweepy.error.TweepError as er:
-        print(er.reason)
-    except StopIteration:
-        break
+
+if __name__ == '__main__':
+    ticker = input("Enter stock ticker OR company name: ")
+    tweets_to_open = int(input("How many tweets would you like to retrieve?"))
+    print(get_relevant_tweets(ticker, tweets_to_open))
