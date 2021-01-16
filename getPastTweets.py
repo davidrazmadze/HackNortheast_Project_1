73adad9da3dd617c6b134d7d91df6ef365ce8d9b
Daniel Tkachov <Daniel_Tkachov@student.uml.edu>
2021-01-15 20:38:09 -0500

    past tweets test

---
+++ getPastTweets.py
@@ -0,0 +1,30 @@
+import tweepy
+import time
+from keys import consumer_key
+from keys import consumer_secret
+from keys import key
+from keys import secret
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(key, secret)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+class tweety(tweepy.StreamListener):
+    def __init__(self, text1, likes1, retweets1, user1, follower1):
+        text = text1                # text of tweet
+        likes = likes1              # number of likes tweet has
+        retweets = retweets1        # number of retweets it has
+        user = user1                # username of tweeter
+        follower = follower1        # follower count of user who posted tweet
+
+ticker = input("Enter Stock Ticker: ")
+numTweet = 5
+for tweet in tweepy.Cursor(api.search, ticker, lang='en').items(numTweet):
+    try:
+        print(f"Tweet received; Text: \n{tweet.text}\n{tweet.user.screen_name}")
+        if ticker in tweet.text:
+            print(f"{ticker} FOUND\n\n")
+    except tweepy.error.TweepError as er:
+        print(er.reason)
+    except StopIteration:
+        break
